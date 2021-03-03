40eb24ae4b9a102a0536ea8647eaf84bfcf5b46a
MurdaWitness <gavrishin.@yandex.ru>
2021-03-03 21:21:50 +0300

    Доработан алгоритм сортировки выбором.

--- choice.c
+++ choice.c
@@ -27,6 +27,7 @@ for (int i=0; i<n; i++)
 	X[i] = rand()%99 + 1;
 }
 
+
 void show(int X[], int n)
 {
 for (int i=0; i<n; i++)
@@ -34,7 +35,25 @@ for (int i=0; i<n; i++)
 printf("\n");
 }
 
+
 void sort(int X[],int n)
 {
+	int tmp;
+	int tmn;
 
+	for (int i = 0; i<n-1; i++)
+	{
+		tmp = X[i];
+		tmn = i;
+		for (int j=i; j<n; j++)
+		{
+			if(X[j]<tmp)
+			{
+				tmp = X[j];
+				tmn = j;
+			}
+		}
+		X[tmn] = X[i];
+		X[i] = tmp;
+	}
 }
