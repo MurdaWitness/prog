db4b108994485f6cd7bcbcb5151d976c94156465
MurdaWitness <gavrishin.a@yandex.ru>
2021-03-11 23:35:42 +0300

    Решил q6_1.c

--- choice.c
+++ choice.c
@@ -1,11 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include "choice.h"
 #define SIZE 10
-
-void fill(int [], int);
-void show(int[], int);
-void sort(int [],int);
 
 int main()
 {
@@ -17,43 +11,4 @@ int main()
 	show(arr, SIZE);
 
 	return 0;
-}
-
-
-void fill(int X[], int n)
-{
-	srand(time(NULL));
-	for (int i=0; i<n; i++)
-	X[i] = rand()%99 + 1;
-}
-
-
-void show(int X[], int n)
-{
-	for (int i=0; i<n; i++)
-	printf("%-2d\t", X[i]);
-	printf("\n");
-}
-
-
-void sort(int X[],int n)
-{
-	int tmp;
-	int tmn;
-
-	for (int i = 0; i<n-1; i++)
-	{
-		tmp = X[i];
-		tmn = i;
-		for (int j=i; j<n; j++)
-		{
-			if(X[j]<tmp)
-			{
-				tmp = X[j];
-				tmn = j;
-			}
-		}
-		X[tmn] = X[i];
-		X[i] = tmp;
-	}
 }
