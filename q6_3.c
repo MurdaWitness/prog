946038658cb7dafed418636e2779d55e165bdbcd
MurdaWitness <gavrishin.a@yandex.ru>
2021-03-13 19:50:05 +0300

    Кайфовый q6_3.c

--- q6_3.c
+++ q6_3.c
@@ -2,28 +2,34 @@
 #define SIZE1 5
 #define SIZE2 7
 
+void chess(int [][SIZE2], int, int);
 void show_2d(int [][SIZE2], int, int);
 
 int main()
 {
     int A[SIZE1][SIZE2];
-    for(int i = 0; i<SIZE1; i+=2)
-    {
-        for(int j = 0; j<SIZE2; j+=2) 
-        A[i][j] = 1;
-        for(int j = 1; j<SIZE2; j+=2) 
-        A[i][j] = 0;
-    }
-
-    for(int i = 1; i<SIZE1; i+=2)
-    {
-        for(int j = 0; j<SIZE2; j+=2) 
-        A[i][j] = 0;
-        for(int j = 1; j<SIZE2; j+=2) 
-        A[i][j] = 1;
-    }
-
+    chess(A, SIZE1, SIZE2);
     show_2d(A, SIZE1, SIZE2);    
+}
+
+
+void chess(int A[][SIZE2], int S1, int S2)
+{
+for(int i = 0; i<S1; i+=2)
+    {
+        for(int j = 0; j<S2; j+=2) 
+        A[i][j] = 1;
+        for(int j = 1; j<S2; j+=2) 
+        A[i][j] = 0;
+    }
+
+for(int i = 1; i<S1; i+=2)
+    {
+        for(int j = 0; j<S2; j+=2) 
+        A[i][j] = 0;
+        for(int j = 1; j<S2; j+=2) 
+        A[i][j] = 1;
+    }
 }
 
 
