a807b551145535686af4f6e2a9ae5241dafa66cd
MurdaWitness <73883504+MurdaWitness@users.noreply.github.com>
2021-03-13 08:50:49 +0000

    Удовл

--- q6_3.c
+++ q6_3.c
@@ -1,38 +1,39 @@
 #include <stdio.h>
-#define SIZE 5
+#define SIZE1 5
+#define SIZE2 7
 
-void show_2d(int [][SIZE], int);
+void show_2d(int [][SIZE2], int, int);
 
 int main()
 {
-    int A[SIZE][SIZE];
-    for(int i = 0; i<SIZE; i+=2)
+    int A[SIZE1][SIZE2];
+    for(int i = 0; i<SIZE1; i+=2)
     {
-        for(int j = 0; j<SIZE; j+=2) 
+        for(int j = 0; j<SIZE2; j+=2) 
         A[i][j] = 1;
-        for(int j = 1; j<SIZE; j+=2) 
+        for(int j = 1; j<SIZE2; j+=2) 
         A[i][j] = 0;
     }
 
-    for(int i = 1; i<SIZE; i+=2)
+    for(int i = 1; i<SIZE1; i+=2)
     {
-        for(int j = 0; j<SIZE; j+=2) 
+        for(int j = 0; j<SIZE2; j+=2) 
         A[i][j] = 0;
-        for(int j = 1; j<SIZE; j+=2) 
+        for(int j = 1; j<SIZE2; j+=2) 
         A[i][j] = 1;
     }
 
-    show_2d(A, SIZE);    
+    show_2d(A, SIZE1, SIZE2);    
 }
 
 
-void show_2d(int A[][SIZE], int S)
+void show_2d(int A[][SIZE2], int S1, int S2)
 {
-    for (int y = 0; y < S; y++)
+    for (int i = 0; i < S1; i++)
     {
-        for (int k = 0; k < S; k++)
+        for (int j = 0; j < S2; j++)
         {
-            printf("%-2d", A[y][k]);
+            printf("%-2d", A[i][j]);
         }
         printf("\n");
     }
